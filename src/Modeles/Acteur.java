91651f808f99544ddcc835da7b3cb2695a8b0310
Frigone <48713984+Frigone@users.noreply.github.com>
2022-05-04 14:51:53 +0200

    Ajout M

---
+++ src/Modeles/Acteur.java
@@ -0,0 +1,13 @@
+package Modeles;
+
+public interface Acteur {
+	
+	//phase construction
+	public void piocher();
+	public void construire();
+	
+	// phase 2
+	public void jouer();
+	public void volerPiece();
+	
+}
