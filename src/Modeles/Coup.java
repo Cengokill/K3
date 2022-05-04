0ff795cd83aa4130faeb8fa6e8bd3b5cc0613420
Frigone <48713984+Frigone@users.noreply.github.com>
2022-05-04 15:06:46 +0200

    ajout M

---
+++ src/Modeles/Coup.java
@@ -0,0 +1,12 @@
+package Modeles;
+
+public class Coup {
+	public Piece piece;
+	public Position p;
+	
+	public Coup(Piece piece, Position position) {
+		this.piece = piece;
+		this.p = position;
+	}
+	
+}
