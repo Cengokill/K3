fa9a8728289118789e0032b652ca16f39ba04355
Killian Cengo <cengokillian@hotmail.com>
2022-05-04 14:33:11 +0200

    classe jeu

---
+++ src/Modeles/Jeu.java
@@ -0,0 +1,30 @@
+package Modeles;
+
+public class Jeu {
+	private Joueur j1;
+	private Joueur j2;
+	
+	public Jeu() {
+		
+	}
+	
+	public void sauverPartie() {
+		
+	}
+	
+	public void chargerPartie() {
+		
+	}
+	
+	public void jouer() {
+		
+	}
+	
+	public Joueur Joueur1() {
+		return j1;
+	}
+	
+	public Joueur Joueur2() {
+		return j2;
+	}
+}
