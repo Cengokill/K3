4d4b1117fd7f81ad6670ad2091dc229bca46a655
remitrev <94910203+remitrev@users.noreply.github.com>
2022-05-04 15:25:11 +0200

    Update IAjeuAlea.java

--- src/Controleur/IAjeuAlea.java
+++ src/Controleur/IAjeuAlea.java
@@ -1,7 +1,18 @@
 package Controleur;
 
+import Modeles.*;
+
 public class IAjeuAlea implements IAjeu {
     // recupere la pyramide du joueur
     // recupere la pyramide du adverse
     // recupere la pyramide du jeu
+    Jeu kTrois;
+
+    IAjeuAlea(Jeu kTrois) {
+        this.kTrois = kTrois;
+    }
+
+    public Sequence<Piece> GetPiecesJouables() {
+
+    }
 }
