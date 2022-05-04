b82dd6a94e5d0cf0c5258820f5622b8452fc5622
remitrev <94910203+remitrev@users.noreply.github.com>
2022-05-04 16:03:31 +0200

    Specifiaction de l'IA Jeu Expert

--- src/Controleur/IAjeuExpert.java
+++ src/Controleur/IAjeuExpert.java
@@ -1,5 +1,22 @@
 package Controleur;
 
 public class IAjeuExpert implements IAjeu {
+    // Cloner le jeu actuel pour travailler dessus
+    // Recupere les coups jouables par les deux joueurs
+    // CAS BASE: j1 gagne si j2 ne peut pas poser et inversement
+    // 2 fonctions
+    // Calcul_j1(c'est a j1 de jouer et renvoie vrai si il gagne) !!j1 est le joueur
+    // IA
+    // Calcul_j2(c'est a j1 de jouer et renvoie vrai si j1 gagne)
 
+    // Calcul_j1 renvoie vrai si dans un de ses coups possibles il est sur de gagner
+    // (OU)
+    // Calcul_j2 renvoie vrai si dans chacun de ses coups possibles j1 gagne (ET)
+
+    // Renvoyer le coup a jouer, si la config est perdante renvoie un coup aleatoire
+    // sinon on renvoie le coup gagnant !!!si min max si perdant pas oblige de
+    // renvoyer un aleatoire
+
+    // !!!prendre en compte le vol de piece et joker et passe tour dans les coups
+    // jouables e
 }
