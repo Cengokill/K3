220a6ef4f9e247ad6d087b84989c50dbb60733b6
remitrev <94910203+remitrev@users.noreply.github.com>
2022-05-05 14:54:44 +0200

    test

--- src/Modeles/Position.java
+++ src/Modeles/Position.java
@@ -1,12 +1,17 @@
 package Modeles;
 
 public class Position {
-	public int x;
-	public int y;
-	
-	public Position(int i, int j)
-    {
-        x=i;
-        y=j;
+    public int x;
+    public int y;
+
+    public Position(int i, int j) {
+        x = i;
+        y = j;
+    }
+
+    public String toString() {
+        String res = "";
+        res += "[ " + x + ", " + y + " ]";
+        return res;
     }
 }
