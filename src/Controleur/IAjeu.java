c64ec06d618faa9d7bd89c2fb8e2c124c038b27c
remitrev <94910203+remitrev@users.noreply.github.com>
2022-05-05 14:01:36 +0200

    Update IAjeu.java

--- src/Controleur/IAjeu.java
+++ src/Controleur/IAjeu.java
@@ -5,6 +5,6 @@ public interface IAjeu {
     // moyen: min-max avec moins de profondeur
     // expert: min-max juqu'au bout
 
-    // Heuristique de base: si notre nb de coups <0 -> +infini
+    // Heuristique de base: si notre nb de coups =0 -> +infini
     // sinon le nombre de coups de l 'adversaire
 }
