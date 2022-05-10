4112b6f01c4e624a3c9e91ebf7c568a2dcffa029
Frigone <48713984+Frigone@users.noreply.github.com>
2022-05-10 13:58:04 +0200

    modification et regroupement des fonctions de base

--- src/Modeles/Acteur.java
+++ src/Modeles/Acteur.java
@@ -4,21 +4,81 @@ import java.util.ArrayList;
 
 public interface Acteur {
 
-	// phase construction
-	public Piece piocherPiece(ArrayList<Piece> sac);
+	private String nom;
+	private PyramideJoueur campJ;
+	private ArrayList<Piece> piecesVolees;// pieces que le joueur a volees a l'autre joueur
+	private ArrayList<Piece> piecesPiochees;// pieces que le joueur a piochees et pas encore placees sur son camp
+	
+	public String getNom() {// renvoie le nom du joueur
+		return this.nom;
+	}
 
-	// phase 2
+	public PyramideJoueur getCamp() {
+		return this.campJ;
+	}
+	
+	//piece Volees
+	
+	public ArrayList<Piece> getPiecesVolees() {
+		return this.piecesVolees;
+	}
+	
+	public void addPiecesPiochees(Piece p) {
+		this.piecesPiochees.add(p);
+	}
+	
+	public String toStringPiecesVolees() {
+		String tableau = new String();
+		for (int i = 0; i < piecesVolees.size(); i++) {
+			tableau += piecesVolees.get(i).toString();
+		}
+		return tableau;
+	}
+	
+	//piece Piochees
+	
+	public ArrayList<Piece> getPiecesPiochees() {
+		return this.piecesPiochees;
+	}
+
+	public void addPieceVolee(Piece p) {
+		this.piecesVolees.add(p);
+	}
+	
+	public int getTaillePiecesPiochees() {
+		return piecesPiochees.size();
+	}
+	
+	public Piece piocherPiece(ArrayList<Piece> sac) {//choisis une pièce dans le sac
+		Piece pChoisie;
+		boolean ColorPresent;
+		if(0>=sac.size()) {
+			System.err.println("erreur sac vide");
+		else if(piecesPiochees.size()<4{ //on n'as pas encore les 4 couleurs différentes
+			ColorPresent = false;
+			int j = 0;
+			while( j<sac.size() && !ColorPresent) {
+				pChoisie = sac.get(j);
+				ColorPresent = false;
+				int i = 0;
+				while(i < piecesPiochees.size() && !ColorPresent) {
+					if(piecesPiochees.get(i).getColor == pChoisie.getColor()) {
+						ColorPresent = true;
+					}
+					i++;
+				}
+				j++;
+			}
+		}else {
+			pChoisie = sac.get(0);
+		}
+	    sac.remove(pChoisie);
+	    return pChoisie;
+	}
+
+	// phase GAMEPLAY
 	public PiecePyramide jouer(PyramideMontagne p); // pyramide au milieu
 
-	public String getNom(); // renvoie le nom du joueur
-
-	public PyramideJoueur getCamp();
-
-	public void addPiecesPiochees(Piece p);
-
-	public int getTaillePiecesPiochees();
-
-	public ArrayList<Piece> getPiecesPiochees();
 
 	public void placerPieces();
 
