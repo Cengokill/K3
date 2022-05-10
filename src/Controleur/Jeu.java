ca744a932ff146175f274ac2294bac3d5b1d5f2f
Killian Cengo <cengokillian@hotmail.com>
2022-05-10 10:11:18 +0200

    commentaires

--- src/Controleur/Jeu.java
+++ src/Controleur/Jeu.java
@@ -32,6 +32,7 @@ public class Jeu {
 		}
 		System.out.println(partieEnCours.joueur1().getCamp().toString());
 		System.out.println(partieEnCours.joueur2().getCamp().toString());
+		System.out.println(partieEnCours.getBaseMontagne().toString());
 	}
 	
 	public void piocher() {
