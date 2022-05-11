e97b3940913f29400b337a95beb1c99854cb4f37
remitrev <94910203+remitrev@users.noreply.github.com>
2022-05-11 15:12:57 +0200

    Create MinMax.java

---
+++ src/Controleur/MinMax.java
@@ -0,0 +1,41 @@
+package Controleur;
+
+import Modeles.*;
+
+public class MinMax {
+    int numerojoueur;
+    int profondeur = 10; // profondeur de recherche
+
+    MinMax(int numerojoueur) { // on créer une IA associé à un joueur
+        this.numerojoueur = numerojoueur;
+    }
+
+    public int eval(Partie p) { // Fonction d'évalutaion de la configuration difference entre notre
+                                // nombre de coups jouables et celui de l'adversaire
+        int nbcoupsj1 = p.coupsJouables(p.joueur1()).size();
+        int nbcoupsj2 = p.coupsJouables(p.joueur1()).size();
+        if (numerojoueur == 0) {
+            if (nbcoupsj1 == 0) {
+                return -10000;
+            }
+            if (nbcoupsj2 == 0) {
+                return 10000;
+            }
+            return (nbcoupsj1 - nbcoupsj2);
+        } else {
+            if (nbcoupsj2 == 0) {
+                return -10000;
+            }
+            if (nbcoupsj1 == 0) {
+                return 10000;
+            }
+            return (nbcoupsj2 - nbcoupsj1);
+        }
+
+    }
+
+    public meilleurConfig(Partie p){
+        
+    }
+
+}
