89fa29b7421c9b40f72858d2533250e09971fb8f
remitrev <94910203+remitrev@users.noreply.github.com>
2022-05-19 15:11:19 +0200

    Update IApiocheExpert.java

--- src/Controleur/IApiocheExpert.java
+++ src/Controleur/IApiocheExpert.java
@@ -1,8 +1,6 @@
 package Controleur;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Iterator;
+import java.util.*;
 
 import Modeles.*;
 
@@ -26,7 +24,7 @@ public class IApiocheExpert implements IApioche {
         // creer une liste de position pour savoir quelle position sont deja utilises
         HashSet<Integer> allpos = new HashSet<>();
 
-        // On place nos joker a des endroits deja définis
+        // On place nos joker a des endroits deja dï¿½finis
         aempiler.add(new PiecePyramide(new Piece(Couleurs.NATUREL), new Position(1, 0)));
         aempiler.add(new PiecePyramide(new Piece(Couleurs.BLANC), new Position(1, 4)));
         aempiler.add(new PiecePyramide(new Piece(Couleurs.NATUREL), new Position(3, 2)));
@@ -37,34 +35,28 @@ public class IApiocheExpert implements IApioche {
         allpos.add(PostoIndice(new Position(3, 0)));
 
         // recuperation de la liste des pieces presentes dans la montagne
-        ArrayList<Couleurs> cpres = new ArrayList<>(); // a tester
+        int noir = 0;
+        int bleu = 0;
+        int vert = 0;
+        int rouge = 0;
+        int jaune = 0;
         for (int i = 0; i < 9; i++) {
             Couleurs coul = pm.getPiece(new Position(0, i)).getColor();
             switch (coul) {
                 case NOIR:
-                    if (!cpres.contains(Couleurs.NOIR)) {
-                        cpres.add(Couleurs.NOIR);
-                    }
+                    noir++;
                     break;
                 case VERT:
-                    if (!cpres.contains(Couleurs.VERT)) {
-                        cpres.add(Couleurs.VERT);
-                    }
+                    vert++;
                     break;
                 case ROUGE:
-                    if (!cpres.contains(Couleurs.ROUGE)) {
-                        cpres.add(Couleurs.ROUGE);
-                    }
+                    rouge++;
                     break;
                 case JAUNE:
-                    if (!cpres.contains(Couleurs.JAUNE)) {
-                        cpres.add(Couleurs.JAUNE);
-                    }
+                    jaune++;
                     break;
                 case BLEU:
-                    if (!cpres.contains(Couleurs.BLEU)) {
-                        cpres.add(Couleurs.BLEU);
-                    }
+                    bleu++;
                     break;
                 default:
                     System.out.println("You never should've come here");
@@ -72,41 +64,91 @@ public class IApiocheExpert implements IApioche {
             }
         }
 
-        int nbpib = 0; // nombre de piece mis en bas de la pyramide
         Iterator<Piece> it = piecesIA.iterator();
+        Random r = new Random();
         // System.out.println("On parcours les pieces a placer");
         while (it.hasNext()) { // on itere sur les pieces a poser
             Piece piece = it.next();
             if (piece.getColor() == Couleurs.NATUREL || piece.getColor() == Couleurs.BLANC) {
                 it.remove();
             } else {
-                if (cpres.contains(piece.getColor()) || nbpib >= 5) { // si la piece est dans la montagne on la place
-                                                                      // aléatoirement sinon on la place en bas
+                double proba = r.nextDouble();
+                double chance = 0;
+                Couleurs coul = piece.getColor();
+                switch (coul) {
+                    case NOIR:
+                        chance = noir / 9;
+                        break;
+                    case VERT:
+                        chance = vert / 9;
+                        break;
+                    case ROUGE:
+                        chance = rouge / 9;
+                        break;
+                    case JAUNE:
+                        chance = jaune / 9;
+                        break;
+                    case BLEU:
+                        chance = bleu / 9;
+                        break;
+                    default:
+                        System.out.println("Stop right there criminal scum ! You violated the law");
+                        break;
+                }
+                if (proba < chance) { // on met en haut
                     boolean estajouter = false;
                     for (int etage = 5; etage >= 0; etage--) {
                         for (int rang = 0; rang < (6 - etage); rang++) {
                             Position pos = new Position(etage, rang);
                             if (!allpos.contains(PostoIndice(pos)) && !estajouter) {
-                                if (etage == 0) {
-                                    nbpib++;
-                                }
                                 aempiler.add(new PiecePyramide(piece, pos));
                                 allpos.add(PostoIndice(pos));
                                 estajouter = true;
                             }
                         }
                     }
-                } else { // pas dans la montagne
-                    // revoir
-                    Position pos = new Position(0, nbpib);
-                    if (allpos.contains(PostoIndice(pos))) {
-                        // System.out.println("cette position possede deja une piece");
+                } else { // on met en bas
+                    boolean estajouter = false;
+                    for (int etage = 0; etage < 6; etage++) {
+                        for (int rang = 0; rang < (6 - etage); rang++) {
+                            Position pos = new Position(etage, rang);
+                            if (!allpos.contains(PostoIndice(pos)) && !estajouter) {
+                                aempiler.add(new PiecePyramide(piece, pos));
+                                allpos.add(PostoIndice(pos));
+                                estajouter = true;
+                            }
+                        }
                     }
-                    aempiler.add(new PiecePyramide(piece, pos));
-                    allpos.add(PostoIndice(pos));
-                    nbpib++;
                 }
-                it.remove();
+                ////////////////////////////////
+                // if (cpres.contains(piece.getColor()) || nbpib >= 5) { // si la piece est dans
+                //////////////////////////////// la montagne on la place
+                // // aleatoirement sinon on la place en bas
+                // boolean estajouter = false;
+                // for (int etage = 5; etage >= 0; etage--) {
+                // for (int rang = 0; rang < (6 - etage); rang++) {
+                // Position pos = new Position(etage, rang);
+                // if (!allpos.contains(PostoIndice(pos)) && !estajouter) {
+                // if (etage == 0) {
+                // nbpib++;
+                // }
+                // aempiler.add(new PiecePyramide(piece, pos));
+                // allpos.add(PostoIndice(pos));
+                // estajouter = true;
+                // }
+                // }
+                // }
+                // } else { // pas dans la montagne
+                // // revoir
+                // Position pos = new Position(0, nbpib);
+                // if (allpos.contains(PostoIndice(pos))) {
+                // // System.out.println("cette position possede deja une piece");
+                // }
+                // aempiler.add(new PiecePyramide(piece, pos));
+                // allpos.add(PostoIndice(pos));
+                // nbpib++;
+                // }
+                // it.remove();
             }
 
         }
