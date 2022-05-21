23ed0038a4d01ec0f862d7ade37457c0c871b0c0
remitrev <94910203+remitrev@users.noreply.github.com>
2022-05-21 12:38:18 +0200

    coupure pas ok a retravailler

--- src/Controleur/IAjeuExpert.java
+++ src/Controleur/IAjeuExpert.java
@@ -16,12 +16,12 @@ public class IAjeuExpert implements IAjeu {
 
     public Coup IACoup(Partie p, int numeroJoueur) {
         MinMax mm = new MinMax(numeroJoueur);
-        mm.meilleurConfigJ(p, horizon, true, -10001);
+        mm.meilleurConfigJ(p, horizon, true);
         avoler = mm.PieceAVoler();
         return mm.getparfait();
     }
 
-    public PiecePyramide PieceAVoler(ArrayList<PiecePyramide> arr) { //Ajouter table de hashage avec une partie et la piece a voler dans ce cas la
+    public PiecePyramide PieceAVoler(ArrayList<PiecePyramide> arr) {
         if (avoler == null) {
             Random r = new Random();
             int index = r.nextInt(arr.size());
