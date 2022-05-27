b906a64e500be6a785964d72a057ddf2f35f1f4b
Killian Cengo <cengokillian@hotmail.com>
2022-05-27 11:47:48 +0200

    boutons suivant et précédent

--- src/Main.java
+++ src/Main.java
@@ -32,7 +32,7 @@ public class Main {
 		
 		Chargement chargement = new Chargement();
 		InitPartie partie = new InitPartie();
-		SoundPlayer simpleSoundPlayerMusic = new SoundPlayer(0);
+		SoundPlayer simpleSoundPlayerMusic = new SoundPlayer(3);
 		simpleSoundPlayerMusic.setNumSon(43);
 		simpleSoundPlayerMusic.loopSon();
 		TypeFenetre prochaineFenetre = chargement.getProchaineFenetre();
