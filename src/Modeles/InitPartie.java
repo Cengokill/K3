0167d8dfc3171dddf77799f3c55e98aca1b5976c
Frigone <48713984+Frigone@users.noreply.github.com>
2022-05-31 17:53:22 +0200

    update

--- src/Modeles/InitPartie.java
+++ src/Modeles/InitPartie.java
@@ -2,6 +2,8 @@ package Modeles;
 
 public class InitPartie {
 	public boolean paramCharges=false;
+	public boolean NewPhase1=true;
+	public boolean NewPhase2=true;
 	public String nomFichierCharge=null;
 	public int modeDeJeu, difficulteIA1, difficulteIA2, vitesseIA;
 	public String nomJoueur1, nomJoueur2;
