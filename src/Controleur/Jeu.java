1d41816e8f893223d2b32d790f024ea579bcfed8
remitrev <94910203+remitrev@users.noreply.github.com>
2022-06-02 00:28:40 +0200

    victoire maj

--- src/Controleur/Jeu.java
+++ src/Controleur/Jeu.java
@@ -256,6 +256,7 @@ public class Jeu {
 			temps = this.vitesseIA;
 		}
 		Acteur jCourant, jPrecedent;
+		partieEnCours.afficheVictoire = false;
 		if (this.partieEnCours.joueurCourant == 0) {
 			jCourant = this.partieEnCours.joueur1();
 			jPrecedent = this.partieEnCours.joueur2();
@@ -327,6 +328,7 @@ public class Jeu {
 			}
 			changement_joueur = true;
 			partieEnCours.changementJoueurCourant();
+			partieEnCours.afficheVictoire = true;
 		}
 	}
 
